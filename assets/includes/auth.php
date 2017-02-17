a97b51ef5a4c79645ae00334d90d468ba05aa377
Filippo Fortino <filippofortino@gmail.com>
2017-02-17 22:56:45 +0100

    No message

--- assets/includes/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$db = new mysqli(HOST, USER, PASS, DATABASE);
-	
-	$feedback = "";
-	
-	if($db->connect_errno > 0)
-		$feedback = 'Error connecting to the database ' . $db->connect_error;
-?>
