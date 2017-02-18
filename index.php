f602da550003093fd90edbab5ed5b1f2ee459d3b
Filippo Fortino <filippofortino@gmail.com>
2017-02-18 18:55:27 +0100

    Added delete and folder creation functionalities

--- index.php
+++ index.php
@@ -20,6 +20,41 @@
 	// Disable browser cache
 	header("Cache-Control: no-cache, must-revalidate");
 	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+	
+	/**
+	 * Create Folders
+	 */
+	if(isset($_POST['create_folder'])) {
+		if($auth->getLoginStatus()) {
+			$folder_name = $_POST['folder_name'];
+			$path = $_POST['folder_path'] . "/$folder_name";
+			$recursive = boolval($_POST['recursive']);
+			
+			if(!(($recursive) ? mkdir($path, 0755, true) : mkdir($path, 0755)))
+				$feedback = "<p class='alert error'>Errore. Impossibile eliminare la cartella.</p>";
+			else
+				$feedback = "<p class='alert success'>Cartella creata correttamente!</p>";
+		} else
+			$feedback = "<p class='alert error'>Errore. Non disponi dei permessi necessari per eseguire questa operazione</p>";
+	}
+	
+	/**
+	 * Delete files
+	 */
+	if(isset($_GET['delete']) && isset($_GET['prev'])) {
+		if($auth->getLoginStatus()) {
+			$file = urldecode($_GET['delete']);
+			$prev = $_GET['prev'];
+			
+			if(!unlink($file))
+				$feedback = "<p class='alert error'>Errore. Impossibile eliminare il file.</p>";
+			else
+				$feedback = "<p class='alert success'>Il file è stato correttamente eliminato!</p>";
+			
+			header("Location: /swap/#$prev");
+		} else
+			$feedback = "<p class='alert error'>Errore. Non disponi dei permessi necessari per eseguire questa operazione</p>";
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -77,25 +112,39 @@
 		<div class="breadcrumbs"></div>
 		<?php 
 			if(isset($feedback)) echo $feedback;
-			if($auth->getLoginStatus()) 
-				echo "Bentronato " . $_SESSION['username'];
 			
 		?>
 		<div id="content">
 			<aside>
-				<input type="checkbox" id="upload" />
-				<label for="upload" id="upload-button"><i class="fa fa-arrow-up"></i>Upload</label>
+				<input type="checkbox" id="upload" name="check[1][]" />
+				<label for="upload" class="nav-button" id="upload-button"><i class="fa fa-arrow-up"></i>Upload</label>
 
-				<form id="upload-form" method="post" action="" enctype="multipart/form-data">
+				<form class="nav-form" id="upload-form" method="post" action="" enctype="multipart/form-data">
 	                <input type="file" name="upl[]" id="file-uploader" multiple /><br />
 	                <ul id="files-list"></ul>
 	                <input type="submit" value="Carica" />
-	                <input type="hidden" id="dir" name="dir" value="" />
+	                <input type="hidden" class="dir" name="dir" value="" />
 				</form>
+				<?php if($auth->getLoginStatus()): ?>
+				<input type="checkbox" id="folder" name="check[1][]" />
+				<label for="folder" class="nav-button" id="dir-button"><i class="fa fa-folder"></i>Nuova cartella</label>
+				
+				<form class="nav-form" id="folder-form" action="" method="post">
+					<input type="text" id="foldername" name="folder_name" placeholder="Nome cartella">
+					
+					<div id="recursive-wrapper">
+						<label for="recursive">Creazione ricorsiva</label>
+						<input type="checkbox" id="recursive" name="recursive" value="true">
+					</div>
+					
+	                <input type="submit" name="create_folder" value="Crea" />
+	                <input type="hidden" class="dir" name="folder_path" value="" />
+				</form>
+				<?php endif; ?>
 			</aside>
 			<div style="clear: both"></div>
 
-			<ul class="data"></ul>
+			<?php echo ($auth->getLoginStatus()) ? "<ul class='data loggedin'></ul>" : "<ul class='data'></ul>"; ?>
 
 			<div class="nothingfound six">
 				<div class="nofiles"></div>
@@ -105,7 +154,10 @@
 	</div>
 
 	<footer>
-        <p>Swap. Made with <i class="fa fa-heart"></i> by <a href="mailto:filippofortino@gmail.com">Filippo Fortino</a></p>
+        <p>
+	        Swap. Made with <i class="fa fa-heart"></i> by <a href="mailto:filippofortino@gmail.com">Filippo Fortino</a>
+			<?php echo ($auth->getLoginStatus()) ? "<span>" . $_SESSION['username'] . " | <a href='?action=logout'>Logout</a></span>" : "<span><a href='admin/'>Admin</a></span>"; ?>	
+	    </p>
     </footer>
 
 	<!-- JS -->
