23feb65d15391a7c7bba7afdd1dedaf808d05f9d
Filippo Fortino <filippofortino@gmail.com>
2017-04-06 23:29:45 +0200

    Added authentication

--- webdav/server.php
+++ webdav/server.php
@@ -1,15 +1,15 @@
 <?php
-
 	use Sabre\DAV;
 	use Sabre\DAV\Auth;
 	
-	// The autoloader
+	date_default_timezone_set('Europe/Rome');
+	
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/swap/assets/includes/config.php';
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/swap/assets/includes/SabreDAV/vendor/autoload.php';
 	
 	$rootDirectory = new DAV\FS\Directory('../Home');
 	$server = new DAV\Server($rootDirectory);
-	$server->setBaseUri('/swap/webdav/server.php');
+	$server->setBaseUri('/swap/webdav/');
 	
 	// The lock manager is reponsible for making sure users don't overwrite
 	// each others changes.
@@ -22,6 +22,8 @@
 	$authBackend = new Auth\Backend\PDO($pdo);
 	$authBackend->setRealm('SabreDAV');
 	$authPlugin = new Auth\Plugin($authBackend);
+	
+	// Adding the plugin to the server.
 	$server->addPlugin($authPlugin);
 	
 	// This ensures that we get a pretty index in the browser, but it is
