3370df133268705116e7cb2fa7d175c41e12a8f7
Filippo Fortino <filippofortino@gmail.com>
2017-06-09 22:28:46 +0200

    Added folder creation

--- assets/includes/FileSystem.class.php
+++ assets/includes/FileSystem.class.php
@@ -8,6 +8,10 @@
 			if(isset($_GET['delete'])) {
 				$this->deleteFile($_GET['delete']);
 			}
+			
+			if(isset($_POST['create_folder'])) {
+				$this->createFolder($_POST['folder_name'], $_POST['folder_path'], boolval($_POST['recursive']));
+			}
 		}
 		
 		
@@ -18,5 +22,14 @@
 				$response = "Impossibile eliminare il file";
 			else
 				$response = "Il file è stato correttamente eliminato!";
+		}
+		
+		private function createFolder($name, $path, $recursive = false) {
+			$path = "$path/$name";
+
+			if(!(($recursive) ? mkdir($path, 0755, true) : mkdir($path, 0755)))
+				$feedback = "<p class='alert error'>Errore. Impossibile creare la cartella.</p>";
+			else
+				$feedback = "<p class='alert success'>Cartella creata correttamente!</p>";
 		}
 	}
