47d381d9c9c329ed1fc9eb4c694b08dfa32ec538
Filippo Fortino <filippofortino@gmail.com>
2017-06-12 10:04:53 +0200

    Add stay logged in checkbox

--- login/index.php
+++ login/index.php
@@ -80,6 +80,9 @@
 							<label for="password">Password</label>
 							<input type="password" id="input--password" name="password">
 							
+							<label for="stay-logged-in">Rimani connesso</label>
+							<input type="checkbox" id="input--stay-logged-in" name="stay-logged-in">
+							
 							<input type="submit" id="input--submit" name="login" value="Accedi">
 						</form>
 					</div>
