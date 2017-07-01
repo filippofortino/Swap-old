24878cab98802d88ca2db1933daf0626764654fe
Filippo Fortino <filippofortino@gmail.com>
2017-07-01 17:20:44 +0200

    Add favicon

--- profile/index.php
+++ profile/index.php
@@ -56,7 +56,9 @@
 				}
 			</style>
 		<![endif]-->
-	
+		
+		<link rel="shortcut icon" href="/swap/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/swap/favicon.ico" type="image/x-icon">
 	</head>
 	<body id="swap">
 		<div class="filemanager">
